169a389b6bb109e1d592331627222340ce5f6972
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-09-20 13:08:54 +0800

    [RSpec] Irozuku context when using built-in colors, it colors the text based on built-in color name

--- spec/irozuku_spec.rb
+++ spec/irozuku_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe Irozuku do
     ansi = Irozuku.hex_to_ansi(Irozuku::Constants::HEX_COLOR_MAP["emerald"])
     expect(ansi.match?(/(\d*);(\d*);(\d*)/)).to eq(true)
   end
+
+  context "when using built-in colors" do
+    Irozuku::Constants::HEX_COLOR_MAP.each do |key, value|
+      it "colors the text #{Irozuku.bold.public_send(key, key)}" do
+        if Irozuku.respond_to?(key)
+          output = Irozuku.public_send(key, "Irozuku")
+        end
+        expect { print output }.to output(output).to_stdout
+      end
+    end
+  end
 end
