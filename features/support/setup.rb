50c2811447301e3d489b77f09e86f763c6a073da
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-09-20 15:03:11 +0800

    [Change] Add coverage support to cucumber

--- features/support/setup.rb
+++ features/support/setup.rb
@@ -1,2 +1,10 @@
+require 'simplecov'
+# https://github.com/simplecov-ruby/simplecov/issues/741#issuecomment-655803917
+Pathname.new(__FILE__).parent.join("..", "..", "coverage").tap do |cov|
+  # clear old coverage results
+  FileUtils.rm_rf cov if cov.exist?
+end
+SimpleCov.start
+
 # https://github.com/cucumber/aruba/tree/main/features/
 require "aruba/cucumber"
