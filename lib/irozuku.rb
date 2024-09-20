c21f912f557edddeafe1ef859119558ff18f8948
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-09-20 12:59:45 +0800

    [Change] Define class method write and cleanup

--- lib/irozuku.rb
+++ lib/irozuku.rb
@@ -5,4 +5,17 @@ require_relative "irozuku/version"
 module Irozuku
   attr_accessor :ansi_color, :ansi_background_color, :ansi_text_decoration, :text
   @ansi_text_decoration = []
+
+  def self.write(string)
+    output = "#{@ansi_color}#{@ansi_background_color}#{@ansi_text_decoration.join("")}#{string}#{"\x1b[0m" if [@ansi_color, @ansi_background_color, @ansi_text_decoration[0]].compact.length > 0}"
+    cleanup
+    output
+  end
+
+  def self.cleanup
+    @ansi_color = nil
+    @ansi_background_color = nil
+    @ansi_text_decoration = []
+    @text = nil
+  end
 end
