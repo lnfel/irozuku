66f8493ff29f2e0fd20d84633df433408f6038cb
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-09-21 08:43:30 +0800

    [Feature] Utils module and silence_stdout method

---
+++ lib/irozuku/utils.rb
@@ -0,0 +1,29 @@
+require "irozuku"
+
+module Irozuku
+  module Utils
+    ##
+    # Silence STDOUT
+    # Usage:
+    # ```ruby
+    # capture_output = silence_stdout do
+    #   # Does not output anything directly.
+    #   puts "Hey!"
+    # end
+    # puts capture_output #=> "Hey!"
+    # ```
+    # https://stackoverflow.com/questions/4459330/how-do-i-temporarily-redirect-stderr-in-ruby/4459463#4459463
+    # https://github.com/simplecov-ruby/simplecov/issues/992#issuecomment-1230389720
+    def self.silence_stdout
+      # The output stream must be an IO-like object. In this case we capture it in
+      # an in-memory IO object so we can return the string value. You can assign any
+      # IO object here.
+      previous_stdout, $stdout = $stdout, StringIO.new
+      yield
+      $stdout.string
+    ensure
+      # Restore the previous value of stdout (typically equal to STDOUT)
+      $stdout = previous_stdout
+    end
+  end
+end
