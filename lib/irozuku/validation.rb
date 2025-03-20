ca2122ea4fb6a9ac5b22ed58604772d742b615f0
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2025-03-20 23:50:12 +0800

    [Change] Improve rdoc comments

--- lib/irozuku/validation.rb
+++ lib/irozuku/validation.rb
@@ -20,19 +20,19 @@ module Irozuku
     end
 
     # Validate hex color string
-    #   ```
-    #   #? - Optionally start with a hash symbol
-    #   (
-    #     ?: - Non-capturing group
-    #     \h - a shorthand character class for hex digit character ([0-9a-fA-F])
-    #     {3} - match the previous token (the hex digit) three times
-    #   )
-    #   {1,2} - repeat either once or twice
-    #   ```
+    # ```
+    # #? - Optionally start with a hash symbol
+    # (
+    #   ?: - Non-capturing group
+    #   \h - a shorthand character class for hex digit character ([0-9a-fA-F])
+    #   {3} - match the previous token (the hex digit) three times
+    # )
+    # {1,2} - repeat either once or twice
+    # ```
     #
-    # https://stackoverflow.com/a/59302334/12478479
-    # https://docs.ruby-lang.org/en/3.3/Regexp.html
-    # https://rubyapi.org/3.3/o/regexp
+    # See [Validate RGB hex](https://stackoverflow.com/a/59302334/12478479),
+    # [Ruby Regexp - Ruby-lang](https://docs.ruby-lang.org/en/master/Regexp.html),
+    # [Ruby Regexp - Rubyapi](https://rubyapi.org/3.3/o/regexp)
     def self.hex_color?(hex_string)
       validation_success = hex_string.match?(/^#?(?:\h{3}){1,2}/)
       if !validation_success
